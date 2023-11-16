fb00265528845e35246634f33d73ff9a4056865d
Chetraru Ion <neluchetraru@gmail.com>
2023-11-16 23:37:41 +0100

    Refactor optimize methods into simple functions that can be used outside this file

--- optimize.py
+++ optimize.py
@@ -1,4 +1,15 @@
-def brainfuck_loop_unroll(bf_code, factor=2):
+def brainfuck_loop_unroll(bf_code):
+    """
+    Unrolls all loops in a given Brainfuck code string and returns the optimized code.
+
+    Inputs:
+        bf_code: str, The Brainfuck code string to optimize.
+        mem_count: int, The number of memory cells to analyse.
+
+
+    Returns:
+        str: The optimized Brainfuck code string with all loops unrolled.
+    """
     result = ""
     loop_stack = []
 
@@ -9,7 +20,7 @@ def brainfuck_loop_unroll(bf_code, factor=2):
             if loop_stack:
                 start_index = loop_stack.pop()
                 loop_body = result[start_index:]
-                result += loop_body * (factor - 1)
+                result += loop_body
                 if loop_stack:
                     result += bf_code[start_index + 1 : loop_stack[-1]]
                 result += "]"
@@ -19,111 +30,94 @@ def brainfuck_loop_unroll(bf_code, factor=2):
     return result
 
 
-bf_code = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
-optimized_code = brainfuck_loop_unroll(bf_code, factor=2)
-print("Original:", bf_code)
-print("Optimized:", optimized_code)
-
-
 # Abstract range
-class BrainfuckAnalyzer:
-    def __init__(self, program):
-        self.program = program
-        self.memory_ranges = [
-            set(range(256)) for _ in range(30000)
-        ]  # Assuming 30,000 memory cells
+def memory_range_analysis_1(bf_code, mem_count=20):
+    """
+    Analyzes the memory ranges that each pointer in a Brainfuck program can access
+    after executing the given code.
 
-    def analyze(self):
-        pointer = 0
-        stack = []
+    Inputs:
+        bf_code: str, The Brainfuck code to analyze.
+        mem_count: int, The number of memory cells available in the program.
 
-        for i, char in enumerate(self.program):
-            if char == ">":
-                pointer += 1
-            elif char == "<":
-                pointer -= 1
-            elif char == "+":
-                self.memory_ranges[pointer] = self.memory_ranges[pointer].intersection(
-                    set(range(256))
-                )
-            elif char == "-":
-                self.memory_ranges[pointer] = self.memory_ranges[pointer].intersection(
-                    set(range(256))
-                )
-            elif char == "[":
-                stack.append(i)
-            elif char == "]":
-                start = stack.pop()
-                self.memory_ranges[pointer] = self.memory_ranges[pointer].intersection(
-                    set(
-                        range(
-                            min(self.memory_ranges[pointer]) // abs(i - start),
-                            max(self.memory_ranges[pointer]) // abs(i - start) + 1,
-                        )
+    Returns:
+        A list of sets, where each set represents the memory ranges that a pointer
+        can access after executing the code. The i-th set in the list corresponds
+        to the i-th pointer in the program.
+    """
+    pointer = 0
+    stack = []
+    memory_ranges = [set(range(256)) for _ in range(mem_count)]
+
+    for i, char in enumerate(bf_code):
+        if char == ">":
+            pointer += 1
+        elif char == "<":
+            pointer -= 1
+        elif char == "+":
+            memory_ranges[pointer] = memory_ranges[pointer].intersection(
+                set(range(256))
+            )
+        elif char == "-":
+            memory_ranges[pointer] = memory_ranges[pointer].intersection(
+                set(range(256))
+            )
+        elif char == "[":
+            stack.append(i)
+        elif char == "]":
+            start = stack.pop()
+            memory_ranges[pointer] = memory_ranges[pointer].intersection(
+                set(
+                    range(
+                        min(memory_ranges[pointer]) // abs(i - start),
+                        max(memory_ranges[pointer]) // abs(i - start) + 1,
                     )
                 )
+            )
 
-    def print_results(self):
-        for i, cell_range in enumerate(self.memory_ranges):
-            if i == 10:  # T
-                break
-            print(f"Memory Cell {i}: {min(cell_range)} - {max(cell_range)}")
+    return memory_ranges
 
 
-brainfuck_program = "++++[>++<-]>[<+++++>-]<."
+def memory_range_analysis_2(bf_code, mem_count=20):
+    """
+    Analyzes the memory ranges that each pointer in a Brainfuck program can access
+    after executing the given code.
 
-analyzer = BrainfuckAnalyzer(brainfuck_program)
-analyzer.analyze()
-analyzer.print_results()
+    Inputs:
+        bf_code: str, The Brainfuck code to analyze.
+        mem_count: int, The number of memory cells available in the program.
 
+    Returns:
+        A list of tuple, where each tuple represents the memory ranges that a pointer
+        can access after executing the code. The i-th tuple in the list corresponds
+        to the i-th pointer in the program.
+    """
 
-# Alternative abstract range
-class BrainfuckAnalyzer:
-    def __init__(self, program):
-        self.program = program
-        self.memory_ranges = [
-            (0, 255) for _ in range(30000)
-        ]  # Assuming 30,000 memory cells
+    pointer = 0
+    stack = []
 
-    def analyze(self):
-        pointer = 0
-        stack = []
+    memory_ranges = [(0, 255) for _ in range(mem_count)]
 
-        for i, char in enumerate(self.program):
-            if char == ">":
-                pointer += 1
-            elif char == "<":
-                pointer -= 1
-            elif char == "+":
-                self.memory_ranges[pointer] = self._update_range(
-                    self.memory_ranges[pointer], 1, 1
-                )
-            elif char == "-":
-                self.memory_ranges[pointer] = self._update_range(
-                    self.memory_ranges[pointer], -1, -1
-                )
-            elif char == "[":
-                stack.append(i)
-            elif char == "]":
-                start = stack.pop()
-                self.memory_ranges[pointer] = self._update_range(
-                    self.memory_ranges[pointer], -(i - start), -(i - start)
-                )
-
-    def _update_range(self, current_range, min_change, max_change):
+    def _update_range(current_range, min_change, max_change):
         min_val = max(0, current_range[0] + min_change)
         max_val = min(255, current_range[1] + max_change)
         return (min_val, max_val)
 
-    def print_results(self):
-        for i, (min_val, max_val) in enumerate(self.memory_ranges):
-            if i == 10:
-                return
-            print(f"Memory Cell {i}: Minimum: {min_val}, Maximum: {max_val}")
+    for i, char in enumerate(bf_code):
+        if char == ">":
+            pointer += 1
+        elif char == "<":
+            pointer -= 1
+        elif char == "+":
+            memory_ranges[pointer] = _update_range(memory_ranges[pointer], 1, 1)
+        elif char == "-":
+            memory_ranges[pointer] = _update_range(memory_ranges[pointer], -1, -1)
+        elif char == "[":
+            stack.append(i)
+        elif char == "]":
+            start = stack.pop()
+            memory_ranges[pointer] = _update_range(
+                memory_ranges[pointer], -(i - start), -(i - start)
+            )
 
-
-brainfuck_program = "++++[>++<-]>[<+++++>-]<."
-
-analyzer = BrainfuckAnalyzer(brainfuck_program)
-analyzer.analyze()
-analyzer.print_results()
+    return memory_ranges
