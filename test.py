49155900bdd88eb1ec7768213cf2e1ce64fb8dc1
Chetraru Ion <neluchetraru@gmail.com>
2023-11-17 11:24:41 +0100

    modularize the test function

--- test.py
+++ test.py
@@ -4,61 +4,68 @@ from Devisualizer import Devisualizer
 from optimize import brainfuck_loop_unroll
 import time
 
-# Change these two as needed
-bfp = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--------------------------------------------.>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<++++++++.[++++++++---------]+++."
-number_of_loops = 1000
-
-# Don't touch!!!
-vis = Visualizer(bfp)
-vis.bfp_to_graph_compressed()
-devis = Devisualizer(vis)
-
-devis_bfp = devis.bfp
-loop_unroll_bfp = brainfuck_loop_unroll(bfp)
+from programs import predefined_programs
 
 
-# The process itself
-print(f"Running base program {number_of_loops} times")
+def test(program_name, number_of_loops=100000):
+    bfp = predefined_programs[program_name]
 
-total_runtime = 0
-for i in range(number_of_loops):
-    start = time.time()
-    inte = Interpreter(Memory())
-    inte.run(bfp)
-    end = time.time()
-    runtime = end - start
-    total_runtime += runtime
+    print(f"Running test: {program_name}")
 
-mean_runtime_bfp = total_runtime / number_of_loops
+    # Don't touch!!!
+    vis = Visualizer(bfp)
+    vis.bfp_to_graph_compressed()
+    devis = Devisualizer(vis)
 
-print(f"Mean runtime of base program is {mean_runtime_bfp}")
-print()
+    devis_bfp = devis.bfp
+    loop_unroll_bfp = brainfuck_loop_unroll(bfp)
 
-print(f"Running Devisualized program {number_of_loops} times")
-total_runtime = 0
-for i in range(number_of_loops):
-    start = time.time()
-    inte = Interpreter(Memory())
-    inte.run(devis_bfp)
-    end = time.time()
-    runtime = end - start
-    total_runtime += runtime
+    # The process itself
+    print(f"Running base program {number_of_loops} times")
 
-mean_runtime_devis_bfp = total_runtime / number_of_loops
+    total_runtime = 0
+    for i in range(number_of_loops):
+        start = time.time()
+        inte = Interpreter(Memory())
+        inte.run(bfp)
+        end = time.time()
+        runtime = end - start
+        total_runtime += runtime
 
-print(f"Mean runtime of devisualized program is {mean_runtime_devis_bfp}")
-print()
+    mean_runtime_bfp = total_runtime / number_of_loops
 
-print(f"Running loop unrolled program {number_of_loops} times")
-total_runtime = 0
-for i in range(number_of_loops):
-    start = time.time()
-    inte = Interpreter(Memory())
-    inte.run(loop_unroll_bfp)
-    end = time.time()
-    runtime = end - start
-    total_runtime += runtime
+    print(f"Mean runtime of base program is {mean_runtime_bfp}")
+    print()
 
-mean_runtime_loop_unroll_bfp = total_runtime / number_of_loops
+    print(f"Running Devisualized program {number_of_loops} times")
+    total_runtime = 0
+    for i in range(number_of_loops):
+        start = time.time()
+        inte = Interpreter(Memory())
+        inte.run(devis_bfp)
+        end = time.time()
+        runtime = end - start
+        total_runtime += runtime
 
-print(f"Mean runtime of loop unrolled program is {mean_runtime_loop_unroll_bfp}")
+    mean_runtime_devis_bfp = total_runtime / number_of_loops
+
+    print(f"Mean runtime of devisualized program is {mean_runtime_devis_bfp}")
+    print()
+
+    print(f"Running loop unrolled program {number_of_loops} times")
+    total_runtime = 0
+    for i in range(number_of_loops):
+        start = time.time()
+        inte = Interpreter(Memory())
+        inte.run(loop_unroll_bfp)
+        end = time.time()
+        runtime = end - start
+        total_runtime += runtime
+
+    mean_runtime_loop_unroll_bfp = total_runtime / number_of_loops
+
+    print(f"Mean runtime of loop unrolled program is {mean_runtime_loop_unroll_bfp}")
+
+
+test("Hello World 1")
+test("Hello World 2")
